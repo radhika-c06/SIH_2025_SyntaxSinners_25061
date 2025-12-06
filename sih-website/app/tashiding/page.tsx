5d64d7eb3a73b8caf87044ed45fdf0e00944dfb4
Vidushi Kochhar <vidushi.kochhar.ug24@nsut.ac.in>
2025-12-06 15:34:30 +0530

    Change search icon color to white

--- sih-website/app/tashiding/page.tsx
+++ sih-website/app/tashiding/page.tsx
@@ -735,7 +735,7 @@ export default function TashidingMonastery() {
             {/* Search Bar */}
             <div className="mb-12 max-w-2xl mx-auto">
               <div className="relative">
-                <svg className="absolute left-6 top-1/2 transform -translate-y-1/2 w-5 h-5 text-amber-400" fill="currentColor" viewBox="0 0 20 20">
+                <svg className="absolute left-6 top-1/2 transform -translate-y-1/2 w-5 h-5 text-white" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
                 </svg>
                 <input
