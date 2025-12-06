172ec80bbda3991792165a2c9d6e2666f7f57278
Vidushi Kochhar <vidushi.kochhar.ug24@nsut.ac.in>
2025-12-06 15:56:32 +0530

    Increase Audio Tour video background height

--- sih-website/app/tashiding/page.tsx
+++ sih-website/app/tashiding/page.tsx
@@ -714,7 +714,7 @@ export default function TashidingMonastery() {
       {/* AUDIO TOUR SECTION */}
       <section className="relative w-full py-0" style={{ backgroundColor: '#410704' }}>
         {/* Heading with video background */}
-        <div className="relative w-full py-10 overflow-hidden">
+        <div className="relative w-full overflow-hidden" style={{ paddingTop: '3rem', paddingBottom: '3rem' }}>
           {/* Background video */}
           <video
             src="/audio tour vid.mp4"
