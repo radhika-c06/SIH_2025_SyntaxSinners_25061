466670ff2a694b796dbddf7779a280c6a7b17668
Vidushi Kochhar <vidushi.kochhar.ug24@nsut.ac.in>
2025-12-06 15:59:25 +0530

    Increase Audio Tour video height to 5rem

--- sih-website/app/tashiding/page.tsx
+++ sih-website/app/tashiding/page.tsx
@@ -714,7 +714,7 @@ export default function TashidingMonastery() {
       {/* AUDIO TOUR SECTION */}
       <section className="relative w-full py-0" style={{ backgroundColor: '#410704' }}>
         {/* Heading with video background */}
-        <div className="relative w-full overflow-hidden" style={{ paddingTop: '4rem', paddingBottom: '4rem' }}>
+        <div className="relative w-full overflow-hidden" style={{ paddingTop: '5rem', paddingBottom: '5rem' }}>
           {/* Background video */}
           <video
             src="/audio tour vid.mp4"
